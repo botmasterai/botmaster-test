86493afe6cc3d7a9f72683bfca1859edc223fea7
Jonathan Wickens <jon@humanise.ai>
2017-05-15 14:27:05 +0100

    update tests to work with botmaster 3.0

--- src/botmaster.js
+++ src/botmaster.js
@@ -1,11 +1,11 @@
 const Botmaster = require('botmaster');
 
 const {telegramSettings} = require('./telegram');
-
-const botsSettings = [{telegram: telegramSettings}];
+const { TelegramBot } = require('botmaster-telegram')
 
 const botmaster = () => new Promise( (resolve, reject) => {
-    const botmaster = new Botmaster({botsSettings});
+    const botmaster = new Botmaster();
+    botmaster.addBot(new TelegramBot(telegramSettings))
     botmaster.on('server running', () => resolve(botmaster));
     botmaster.on('error', reject);
 });
